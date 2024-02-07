93f09c3816000411c1bf2451ce6501b9e2e00c30
Lea Kalmar <lea.kalmar@gmail.com>
2024-02-07 11:59:55 +0100

    feat: add toolbar to home

--- src/app/home/components/toolbar/toolbar.component.ts
+++ src/app/home/components/toolbar/toolbar.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-toolbar',
+  host: {
+    class:'toolbar-host-wrapper'
+  },
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatInputModule, MatCardModule, NgOptimizedImage, MatIconModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
+  public minute: string = '';
+  public hour: any;
+  public currentDate: Date = new Date();
 
+  ngOnInit() {
+    this.currentDate = new Date();
+    this.updateDate();
+    setInterval(() => {
+      this.currentDate = new Date();
+      this.updateDate();
+    }, 1000);
+
+  }
+
+
+  private updateDate(){
+    const hours = this.currentDate.getHours();
+    this.hour = hours % 12;
+    this.hour = this.hour ? this.hour : 12;
+
+    this.hour = this.hour < 10 ? '0' + this.hour : this.hour;
+
+    const minutes = this.currentDate.getMinutes();
+    this.minute = minutes < 10 ? '0' + minutes : minutes.toString();
+  }
 }
